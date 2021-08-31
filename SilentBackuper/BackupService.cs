d41b215b7d44159e24c81d8404d8f3aa3c76bbe8
AndrewSotnikovEng <andrew.sotnikov.eng@gmail.com>
2021-08-31 17:37:18 +0300

    Initial commit

---
+++ SilentBackuper/BackupService.cs
@@ -0,0 +1,109 @@
+﻿using IniParser;
+using IniParser.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SilentBackuper
+{
+    class BackupService
+    {
+        FileIniDataParser Parser = new FileIniDataParser();
+        string SettingsFile { get; set; }
+
+        IniData Data { get
+            {
+                return Parser.ReadFile(SettingsFile);
+            } 
+        }
+
+        string Destination
+        {
+            get
+            {
+                return Data["Settings"]["destination"];
+            }
+        }
+
+        public BackupService(string settingsFile)
+        {
+            SettingsFile = settingsFile;
+        }       
+
+
+        public void PrepareMainDestination()
+        {
+            //create main backup destination           
+
+            if (!Directory.Exists(Destination))
+            {
+                Directory.CreateDirectory(Destination);
+            }
+        }
+
+        public  void PrepareMonthFolder()
+        {
+            if (!Directory.Exists(MonthFolderPath))
+            {
+                Directory.CreateDirectory(MonthFolderPath);
+            }
+        }
+
+        string MonthFolderPath
+        {
+            get {
+                return Path.Combine(Destination,
+                    DateTime.Now.ToString("MM") + "__" + DateTime.Now.ToString("MMMM"));
+            }
+        }
+
+        
+        public void DoBackup()
+        {
+            //get backups
+            //if it files, get all files
+            foreach (var key in Data["Settings"])
+            {
+                if (key.KeyName.Contains("source"))
+                {
+                    string sourceFileName = Data["Settings"][key.KeyName];
+                    string zipFilePath = $"{MonthFolderPath}\\" +
+                                            $"{Path.GetFileNameWithoutExtension(sourceFileName)}_" +
+                                            $"{DateTime.Now.Day}_{DateTime.Now.Month}_{DateTime.Now.Year}.zip";
+
+                    CreateZipFile(zipFilePath, sourceFileName);
+                }
+            }
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadKey();
+            //if it dirs
+        }
+
+        public void CreateZipFile(string zipFilePath, string filePath)
+        {
+            if (File.Exists(zipFilePath))
+            {
+                Console.WriteLine($"Path {zipFilePath} already existed");
+                return;
+            }
+            // Create and open a new ZIP file
+            var zip = ZipFile.Open(zipFilePath, ZipArchiveMode.Create);
+            // Add the entry for each file
+            zip.CreateEntryFromFile(filePath, Path.GetFileName(filePath), CompressionLevel.Optimal);
+            Console.WriteLine($"Backup done {zipFilePath}");
+            // Dispose of the object when we are done
+            zip.Dispose();
+        }
+
+        public void PrintHeader()
+        {
+            Console.WriteLine("\n" + string.Concat(Enumerable.Repeat("*", 40)));
+            Console.WriteLine(String.Format("{0,-10}{1,-10}" , " ", DateTime.Now.ToString()));
+            Console.WriteLine(string.Concat(Enumerable.Repeat("*", 40)) + "\n");
+        }
+    }
+}
---
+++ SilentBackuper/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+using IniParser.Model;
+using IniParser;
+
+namespace SilentBackuper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BackupService service = new BackupService("settings.ini");
+
+            service.PrepareMainDestination();
+            service.PrepareMonthFolder();
+            service.PrintHeader();
+            service.DoBackup();
+        }
+
+    }
+}
